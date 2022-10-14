67185c9ab793b4fb83fb9728661ab6a3d2073157
OdiousCode <gorgande@gmail.com>
2022-10-14 15:14:12 +0200

    Added color for every animal + icon. Unified list of both

---
+++ src/constants/Colors.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+  background: "#FFFFFF",
+  primary: "#000000",
+  secondary: "#333333",
+  avatar_Fox: "#FF7E46",
+  avatar_Squid: "#CD5D6F",
+  avatar_Chicken: "#FCD933",
+  avatar_Pig: "#f5997a",
+  avatar_Whale: "#4a9bff",
+  avatar_Frog: "#00a303",
+};
