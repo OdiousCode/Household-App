da03dd971250d1910d37c5ef967fd6efb655d00d
Serover <35572082+Serover@users.noreply.github.com>
2022-10-14 11:11:48 +0200

    start of mockdata

---
+++ src/data/Mockdata.ts
@@ -0,0 +1,19 @@
+// import { Profile, User } from "./APItypes";
+
+// const mockUser: User[] = [
+//   {
+//     email: "fakeEmail@email.com",
+//     id: 1,
+//     password: "bralösenord",
+//   },
+// ];
+
+// const mockProfile: Profile[] = [
+//     {
+
+//     }
+// ]
+
+// export function getUser(email: string, password: string) {
+//   // return mockUser.find((p) => p.id === id);
+// }
