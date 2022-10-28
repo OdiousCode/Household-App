0c64345e59c219d4bd6d1c77c2bdba4eb78104e6
Annangggg <dnnguyen000@gmail.com>
2022-10-28 15:09:29 +0200

    added task

--- src/screens/household/TaskScreen.tsx
+++ src/screens/household/TaskScreen.tsx
@@ -14,35 +14,26 @@ import * as yup from "yup";
 export default function TaskScreen({
   navigation,
 }: HouseholdScreenProps<"TaskScreen">) {
-  const loginValidationSchema = yup.object().shape({
-    email: yup
-      .string()
-      .email("Please enter valid email")
-      .required("Email Address is Required"),
-    password: yup
-      .string()
-      .min(6, ({ min }) => `Password must be at least ${min} characters`)
-      .required("Password is required"),
-  });
+  
   const dispatch = useAppDispatch();
-  //   const balance = useAppSelector((state) => state.bank.balance);
-  //   const transactions = useAppSelector((state) => state.bank.transactions);
-  //   const profile = useAppSelector((state) => state.profile);
-  const [taskName, setTaskName] = useState("");
-  const [taskDescription, setTaskDescription] = useState("");
-  const [taskDifficulty, setTaskDifficulty] = useState('');
-  const [taskFrequency, setTaskFrequency] = useState('');
 
-  const tasko: Task = {
-    id: '0',
-    householdId: '0',
-    description: taskDescription,
-    difficulty: 0,
-    frequency: 0,
-    name: taskName,
-    isArchived: false,
-  }
-
+  const TaskValidationSchema = yup.object().shape({
+      Title: yup
+      .string()
+      .required("Title Address is Required"),
+      Description: yup
+      .string()
+      .required("Deacription is required"),
+      Difficulty: yup
+      .number()
+      .max(5, `Difficulty should be number between 1-5`)
+      .min(1, `Difficulty should be number between 1-5`)
+      .required("Difficulty is Required"),
+      Frequency: yup
+      .number()
+      .min(1, `Frequency should be number between 1-5`)
+      .required("Frequency is Required"),
+  });
 
 
   return (
@@ -51,10 +42,9 @@ export default function TaskScreen({
         <Text style={styles.title}>Skapa en ny syssla</Text>
 
         <Formik
-          //TODO idk where validateOnMount exists
           validateOnChange={true}
-          validationSchema={loginValidationSchema}
-          initialValues={{ email: "", password: "" }}
+          validationSchema={TaskValidationSchema}
+          initialValues={{ Title: "", Description: "", Difficulty: 1, Frequency: "" }}
           onSubmit={(values) => console.log(values)}
         >
           {({
@@ -77,20 +67,19 @@ export default function TaskScreen({
                   Title
                 </Text>
                 <TextInput
-                  placeholder="Email Address"
+                  placeholder="Title"
                   style={styles.input}
-                  onChangeText={handleChange("email")}
-                  onBlur={handleBlur("email")}
-                  value={values.email}
-                  keyboardType="email-address"
+                  onChangeText={handleChange("Title")}
+                  onBlur={handleBlur("Title")}
+                  value={values.Title}
                 />
-                {errors.email && touched.email && (
+                {errors.Title && touched.Title&& (
                   <Text
                     style={{
                       color: "red",
                     }}
                   >
-                    {errors.email}
+                    {errors.Title}
                   </Text>
                 )}
               </View>
@@ -102,47 +91,85 @@ export default function TaskScreen({
                     color: "black",
                   }}
                 >
-                  Password
+                  Description
                 </Text>
                 <TextInput
-                  placeholder="Password"
+                  placeholder="Description"
                   style={styles.input}
-                  onChangeText={handleChange("password")}
-                  onBlur={handleBlur("password")}
-                  value={values.password}
-                  secureTextEntry
+                  onChangeText={handleChange("Description")}
+                  onBlur={handleBlur("Description")}
+                  value={values.Description}
                 />
-                {errors.password && touched.password && (
+                {errors.Description && touched.Description && (
                   <Text
                     style={{
                       color: "red",
                     }}
                   >
-                    {errors.password}
+                    {errors.Description}
+                  </Text>
+                )}
+                 <Text
+                  style={{
+                    fontSize: 17,
+                    fontWeight: "400",
+                    color: "black",
+                  }}
+                >
+                  Difficulty
+                </Text>
+                <TextInput
+                  placeholder="Difficulty"
+                  style={styles.input}
+                  onChangeText={handleChange("Difficulty")}
+                  onBlur={handleBlur("Difficulty")}
+                  value={values.Difficulty.toString()}
+                  maxLength={1}
+                />
+                {errors.Difficulty && touched.Difficulty && (
+                  <Text
+                    style={{
+                      color: "red",
+                    }}
+                  >
+                    {errors.Difficulty}
+                  </Text>
+                )}
+                 <Text
+                  style={{
+                    fontSize: 17,
+                    fontWeight: "400",
+                    color: "black",
+                  }}
+                >
+                  Frequency
+                </Text>
+                <TextInput
+                  placeholder="Frequency"
+                  style={styles.input}
+                  onChangeText={handleChange("Frequency")}
+                  onBlur={handleBlur("Frequency")}
+                  value={values.Frequency}
+                  maxLength={1}
+
+                />
+                {errors.Frequency && touched.Frequency && (
+                  <Text
+                    style={{
+                      color: "red",
+                    }}
+                  >
+                    {errors.Frequency}
                   </Text>
                 )}
               </View>
-            </>
-          )}
-        </Formik>
-
-
-        <View style={{
-          position: "absolute",
-          justifyContent: "space-between",
-          alignItems: "center",
-          flexDirection: "row",
-          width: "100%",
-          bottom: 25,
-          padding: 10,
-        }}>
-          <Button
+              <Button
             icon="plus-circle-outline"
             mode="contained"
             buttonColor="#DCCFCF"
             textColor="#000"
             style={{ borderRadius: 50, borderWidth: 1, width: 150 }}
-            onPress={() => dispatch(createHouseholdTask(tasko))}
+            onPress={() => dispatch(createHouseholdTask({ id:"", householdId: "", name: values.Title, description: values.Description, difficulty: values.Difficulty, frequency: 1, voice: "", img: "", isArchived: false}))}
           >
             Spara
           </Button>
@@ -155,6 +182,20 @@ export default function TaskScreen({
           >
             Stäng
           </Button>
+            </>
+          )}
+
+       
+        </Formik>
+        <View style={{
+          position: "absolute",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          width: "100%",
+          bottom: 25,
+          padding: 10,
+        }}>
         </View>
 
       </SafeAreaView >
