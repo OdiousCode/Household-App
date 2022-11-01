79dd3ba3de6c66d65d4ff1c68f0b123f3867aa9d
Gentrithotii <Gentrithoti@hotmail.com>
2022-11-01 09:38:44 +0100

    First commit

--- src/screens/household/StatisticsScreen.tsx
+++ src/screens/household/StatisticsScreen.tsx
@@ -4,31 +4,118 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { HouseholdScreenProps } from "../../navigation/HouseholdTopTabNavigator";
 // import { setName } from "../store/profileSlice";
 // import { useAppDispatch, useAppSelector } from "../store/store";
+import Svg, { G, Circle } from "react-native-svg";
 
 export default function StatisticsScreen({
   navigation,
 }: HouseholdScreenProps<"StatisticsScreen">) {
-  //   const dispatch = useAppDispatch();
-  //   const balance = useAppSelector((state) => state.bank.balance);
-  //   const transactions = useAppSelector((state) => state.bank.transactions);
-  //   const profile = useAppSelector((state) => state.profile);
+  const radius = 70;
+  const circleCircumference = 2 * Math.PI * radius;
+
+  const groceries = 241;
+  const bills = 350;
+  const regular = 188;
+  const total = groceries + bills + regular;
+
+  const groceriesPercentage = (groceries / total) * 100;
+  const billsPercentage = (bills / total) * 100;
+  const regularPercentage = (regular / total) * 100;
+
+  const groceriesStrokeDashoffset =
+    circleCircumference - (circleCircumference * groceriesPercentage) / 100;
+  const billsStrokeDashoffset =
+    circleCircumference - (circleCircumference * billsPercentage) / 100;
+  const regularStrokeDashoffset =
+    circleCircumference - (circleCircumference * regularPercentage) / 100;
+
+  const groceriesAngle = (groceries / total) * 360;
+  const billsAngle = (bills / total) * 360;
+  const regularAngle = groceriesAngle + billsAngle;
+
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Statistics Screen</Text>
-
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-
-      {/* <Button title="Set name" onPress={() => dispatch(setName("David"))} /> */}
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.graphWrapper}>
+        <Svg height="160" width="160" viewBox="0 0 180 180">
+          <G rotation={-90} originX="90" originY="90">
+            {total === 0 ? (
+              <Circle
+                cx="50%"
+                cy="50%"
+                r={radius}
+                stroke="#F1F6F9"
+                fill="transparent"
+                strokeWidth="40"
+              />
+            ) : (
+              <>
+                <Circle
+                  cx="50%"
+                  cy="50%"
+                  r={radius}
+                  stroke="#F05454"
+                  fill="transparent"
+                  strokeWidth="40"
+                  strokeDasharray={circleCircumference}
+                  strokeDashoffset={groceriesStrokeDashoffset}
+                  rotation={0}
+                  originX="90"
+                  originY="90"
+                  strokeLinecap="round"
+                />
+                <Circle
+                  cx="50%"
+                  cy="50%"
+                  r={radius}
+                  stroke="#30475E"
+                  fill="transparent"
+                  strokeWidth="40"
+                  strokeDasharray={circleCircumference}
+                  strokeDashoffset={billsStrokeDashoffset}
+                  rotation={groceriesAngle}
+                  originX="90"
+                  originY="90"
+                  strokeLinecap='round'
+                />
+                <Circle
+                  cx="50%"
+                  cy="50%"
+                  r={radius}
+                  stroke="#222831"
+                  fill="transparent"
+                  strokeWidth="40"
+                  strokeDasharray={circleCircumference}
+                  strokeDashoffset={regularStrokeDashoffset}
+                  rotation={regularAngle}
+                  originX="90"
+                  originY="90"
+                  strokeLinecap="round"
+                />
+              </>
+            )
+            }
+          </G>
+        </Svg>
+        <Text style={styles.label}>{total}€</Text>
+      </View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  graphWrapper: {
     alignItems: "center",
     justifyContent: "center",
+  },
+  label: {
+    position: "absolute",
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 29,
   },
 });
