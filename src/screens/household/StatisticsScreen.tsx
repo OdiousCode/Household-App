28ae3d71b52ed39375b8c8d1777b5fec98ee5c36
OdiousCode <gorgande@gmail.com>
2022-11-01 13:04:36 +0100

    test av chart

--- src/screens/household/StatisticsScreen.tsx
+++ src/screens/household/StatisticsScreen.tsx
@@ -6,116 +6,75 @@ import { HouseholdScreenProps } from "../../navigation/HouseholdTopTabNavigator"
 // import { useAppDispatch, useAppSelector } from "../store/store";
 import Svg, { G, Circle } from "react-native-svg";
 
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryPie,
+  VictoryTheme,
+} from "victory-native";
+import { avatarColors } from "../../constants/Colors";
+import { getAllAvatars } from "../../constants/Layout";
+
 export default function StatisticsScreen({
   navigation,
 }: HouseholdScreenProps<"StatisticsScreen">) {
-  const radius = 70;
-  const circleCircumference = 2 * Math.PI * radius;
-
-  const groceries = 241;
-  const bills = 350;
-  const regular = 188;
-  const total = groceries + bills + regular;
-
-  const groceriesPercentage = (groceries / total) * 100;
-  const billsPercentage = (bills / total) * 100;
-  const regularPercentage = (regular / total) * 100;
-
-  const groceriesStrokeDashoffset =
-    circleCircumference - (circleCircumference * groceriesPercentage) / 100;
-  const billsStrokeDashoffset =
-    circleCircumference - (circleCircumference * billsPercentage) / 100;
-  const regularStrokeDashoffset =
-    circleCircumference - (circleCircumference * regularPercentage) / 100;
-
-  const groceriesAngle = (groceries / total) * 360;
-  const billsAngle = (bills / total) * 360;
-  const regularAngle = groceriesAngle + billsAngle;
-
-
+  const av = getAllAvatars();
   return (
     <View style={styles.container}>
-      <View style={styles.graphWrapper}>
-        <Svg height="160" width="160" viewBox="0 0 180 180">
-          <G rotation={-90} originX="90" originY="90">
-            {total === 0 ? (
-              <Circle
-                cx="50%"
-                cy="50%"
-                r={radius}
-                stroke="#F1F6F9"
-                fill="transparent"
-                strokeWidth="40"
-              />
-            ) : (
-              <>
-                <Circle
-                  cx="50%"
-                  cy="50%"
-                  r={radius}
-                  stroke="#F05454"
-                  fill="transparent"
-                  strokeWidth="40"
-                  strokeDasharray={circleCircumference}
-                  strokeDashoffset={groceriesStrokeDashoffset}
-                  rotation={0}
-                  originX="90"
-                  originY="90"
-                  strokeLinecap="round"
-                />
-                <Circle
-                  cx="50%"
-                  cy="50%"
-                  r={radius}
-                  stroke="#30475E"
-                  fill="transparent"
-                  strokeWidth="40"
-                  strokeDasharray={circleCircumference}
-                  strokeDashoffset={billsStrokeDashoffset}
-                  rotation={groceriesAngle}
-                  originX="90"
-                  originY="90"
-                  strokeLinecap='round'
-                />
-                <Circle
-                  cx="50%"
-                  cy="50%"
-                  r={radius}
-                  stroke="#222831"
-                  fill="transparent"
-                  strokeWidth="40"
-                  strokeDasharray={circleCircumference}
-                  strokeDashoffset={regularStrokeDashoffset}
-                  rotation={regularAngle}
-                  originX="90"
-                  originY="90"
-                  strokeLinecap="round"
-                />
-              </>
-            )
-            }
-          </G>
-        </Svg>
-        <Text style={styles.label}>{total}€</Text>
+      <View>
+        <Text style={{ fontSize: 22, textAlign: "center" }}>Diska</Text>
+        <VictoryPie
+          width={300}
+          height={300}
+          colorScale={[
+            av[0].color,
+            av[1].color,
+            av[2].color,
+            av[3].color,
+            av[4].color,
+            av[5].color,
+          ]}
+          data={[
+            { x: av[0].icon, y: 33 },
+            { x: av[1].icon, y: 22 },
+            { x: av[2].icon, y: 44 },
+            { x: av[3].icon, y: 55 },
+            { x: av[4].icon, y: 11 },
+            { x: av[5].icon, y: 33 },
+          ]}
+        />
+      </View>
+      <View>
+        <Text style={{ fontSize: 22, textAlign: "center" }}>Tvätta</Text>
+        <VictoryPie
+          width={300}
+          height={300}
+          colorScale={[
+            av[0].color,
+            av[1].color,
+            av[2].color,
+            av[3].color,
+            av[4].color,
+            av[5].color,
+          ]}
+          data={[
+            { x: av[0].icon, y: 5 },
+            { x: av[1].icon, y: 7 },
+            { x: av[2].icon, y: 1 },
+            { x: av[3].icon, y: 2 },
+            { x: av[4].icon, y: 11 },
+            { x: av[5].icon, y: 5 },
+          ]}
+        />
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  graphWrapper: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  label: {
-    position: "absolute",
-    textAlign: "center",
-    fontWeight: "700",
-    fontSize: 29,
   },
 });
